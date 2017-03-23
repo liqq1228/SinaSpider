08ddb627cb70d023e05103cfeb013c7f3cdfa239
Bones <bone_ace@163.com>
2017-03-23 17:16:02 +0800

    更新Cookie的获取途径

--- Sina_spider3/Sina_spider3/cookies.py
+++ Sina_spider3/Sina_spider3/cookies.py
@@ -5,57 +5,84 @@
 #   作者：九茶<http://blog.csdn.net/bone_ace>
 # ------------------------------------------
 
-import base64
 import os
-import requests
+import time
 import json
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import logging
+from yumdama import identify
 
+IDENTIFY = 1  # 验证码输入方式:        1:看截图aa.png，手动输入     2:云打码
+dcap = dict(DesiredCapabilities.PHANTOMJS)  # PhantomJS需要使用老版手机的user-agent，不然验证码会无法通过
+dcap["phantomjs.page.settings.userAgent"] = (
+    "Mozilla/5.0 (Linux; U; Android 2.3.6; en-us; Nexus S Build/GRK39F) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
+)
 logger = logging.getLogger(__name__)
+logging.getLogger("selenium").setLevel(logging.WARNING)  # 将selenium的日志级别设成WARNING，太烦人
 
 """
     输入你的微博账号和密码，可去淘宝买，一元5个。
     建议买几十个，实际生产建议100+，微博反爬得厉害，太频繁了会出现302转移。
 """
 myWeiBo = [
-    ('gaiji690yunhe@163.com', 'XVv2p6kW9k700v4'),
-    ('qiao6570492yata@163.com', '3v91DQ30UhIJim'),
+    ('13467408430', 'aogan571'),
 ]
 
 
 def getCookie(account, password):
     """ 获取一个账号的Cookie """
-    loginURL = "https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.15)"
-    username = base64.b64encode(account.encode("utf-8")).decode("utf-8")
-    postData = {
-        "entry": "sso",
-        "gateway": "1",
-        "from": "null",
-        "savestate": "30",
-        "useticket": "0",
-        "pagerefer": "",
-        "vsnf": "1",
-        "su": username,
-        "service": "sso",
-        "sp": password,
-        "sr": "1440*900",
-        "encoding": "UTF-8",
-        "cdult": "3",
-        "domain": "sina.com.cn",
-        "prelt": "0",
-        "returntype": "TEXT",
-    }
-    session = requests.Session()
-    r = session.post(loginURL, data=postData)
-    jsonStr = r.content.decode("gbk")
-    info = json.loads(jsonStr)
-    if info["retcode"] == "0":
-        logger.warning("Get Cookie Success!( Account:%s )" % account)
-        cookie = session.cookies.get_dict()
+    try:
+        browser = webdriver.PhantomJS(desired_capabilities=dcap)
+        browser.get("https://weibo.cn/login/")
+        time.sleep(1)
+        browser.save_screenshot("aa.png")
+
+        failure = 0
+        while "微博" in browser.title and failure < 5:
+            failure += 1
+            username = browser.find_element_by_name("mobile")
+            username.clear()
+            username.send_keys(account)
+
+            psd = browser.find_element_by_xpath('//input[@type="password"]')
+            psd.clear()
+            psd.send_keys(password)
+            try:
+                code = browser.find_element_by_name("code")
+                code.clear()
+                if IDENTIFY == 1:
+                    code_txt = raw_input("请查看路径下新生成的aa.png，然后输入验证码:")  # 手动输入验证码
+                else:
+                    from PIL import Image
+                    img = browser.find_element_by_xpath('//form[@method="post"]/div/img[@alt="请打开图片显示"]')
+                    x = img.location["x"]
+                    y = img.location["y"]
+                    im = Image.open("aa.png")
+                    im.crop((x, y, 100 + x, y + 22)).save("ab.png")  # 剪切出验证码
+                    code_txt = identify()  # 验证码打码平台识别
+                code.send_keys(code_txt)
+            except Exception, e:
+                pass
+
+            commit = browser.find_element_by_name("submit")
+            commit.click()
+            time.sleep(4)
+
+        cookie = {}
+        if "我的首页" in browser.title:
+            for elem in browser.get_cookies():
+                cookie[elem["name"]] = elem["value"]
+            logger.warning("Get Cookie Success!( Account:%s )" % account)
         return json.dumps(cookie)
-    else:
-        logger.warning("Failed!( Reason:%s )" % info["reason"])
+    except Exception, e:
+        logger.warning("Failed %s!" % account)
         return ""
+    finally:
+        try:
+            browser.quit()
+        except Exception, e:
+            pass
 
 
 def initCookie(rconn, spiderName):
@@ -91,5 +118,5 @@ def removeCookie(accountText, rconn, spiderName):
     cookieNum = "".join(rconn.keys()).count("SinaSpider:Cookies")
     logger.warning("The num of the cookies left is %s" % cookieNum)
     if cookieNum == 0:
-        logger.warning('Stopping...')
+        logger.warning("Stopping...")
         os.system("pause")
