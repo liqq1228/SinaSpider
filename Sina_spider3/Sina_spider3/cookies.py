d29986e549d39a8732a5ddabfd9363d32a9e702f
Tony.TangBo <tangboneu@foxmail.com>
2017-07-20 17:32:13 +0800

    添加cookie的获取方式，原来的cookie获取现在好用了

--- Sina_spider3/Sina_spider3/cookies.py
+++ Sina_spider3/Sina_spider3/cookies.py
@@ -5,7 +5,9 @@
 #   作者：九茶<http://blog.csdn.net/bone_ace>
 # ------------------------------------------
 
+import base64
 import os
+import requests
 import time
 import json
 from selenium import webdriver
@@ -14,6 +16,7 @@ import logging
 from yumdama import identify
 
 IDENTIFY = 1  # 验证码输入方式:        1:看截图aa.png，手动输入     2:云打码
+COOKIE_GETWAY = 0 # 0 代表从https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.18) 获取cookie   # 1 代表从https://weibo.cn/login/获取Cookie
 dcap = dict(DesiredCapabilities.PHANTOMJS)  # PhantomJS需要使用老版手机的user-agent，不然验证码会无法通过
 dcap["phantomjs.page.settings.userAgent"] = (
     "Mozilla/5.0 (Linux; U; Android 2.3.6; en-us; Nexus S Build/GRK39F) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
@@ -31,6 +34,49 @@ myWeiBo = [
 
 
 def getCookie(account, password):
+    if COOKIE_GETWAY == 0:
+        return get_cookie_from_login_sina_com_cn(account, password)
+    elif COOKIE_GETWAY ==1:
+        return get_cookie_from_weibo_cn(account, password)
+    else:
+        logger.error("COOKIE_GETWAY Error!")
+
+def get_cookie_from_login_sina_com_cn(account, password):
+    """ 获取一个账号的Cookie """
+    loginURL = "https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.18)"
+    username = base64.b64encode(account.encode("utf-8")).decode("utf-8")
+    postData = {
+        "entry": "sso",
+        "gateway": "1",
+        "from": "null",
+        "savestate": "30",
+        "useticket": "0",
+        "pagerefer": "",
+        "vsnf": "1",
+        "su": username,
+        "service": "sso",
+        "sp": password,
+        "sr": "1440*900",
+        "encoding": "UTF-8",
+        "cdult": "3",
+        "domain": "sina.com.cn",
+        "prelt": "0",
+        "returntype": "TEXT",
+    }
+    session = requests.Session()
+    r = session.post(loginURL, data=postData)
+    jsonStr = r.content.decode("gbk")
+    info = json.loads(jsonStr)
+    if info["retcode"] == "0":
+        logger.warning("Get Cookie Success!( Account:%s )" % account)
+        cookie = session.cookies.get_dict()
+        return json.dumps(cookie)
+    else:
+        logger.warning("Failed!( Reason:%s )" % info["reason"])
+        return ""
+
+
+def get_cookie_from_weibo_cn(account, password):
     """ 获取一个账号的Cookie """
     try:
         browser = webdriver.PhantomJS(desired_capabilities=dcap)
@@ -73,7 +119,7 @@ def getCookie(account, password):
             if '未激活微博' in browser.page_source:
                 print '账号未开通微博'
                 return {}
-            
+
         cookie = {}
         if "我的首页" in browser.title:
             for elem in browser.get_cookies():
